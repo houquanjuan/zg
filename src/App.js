5fd2599b0b7c0ec7df0c774a1540ddbfd0db0851
houquanjuan <2585061200@qq.com>
2022-03-03 17:26:19 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
-import { Button } from 'antd';
+import { Layout, Menu, Button } from 'antd';
+import { DesktopOutlined } from '@ant-design/icons'
+
+const {Header, Footer, Sider, Content} = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">click</Button>
-    </div>
+   <Layout>
+     <Header>header</Header>
+     <Layout>
+       <Sider>Sider</Sider>
+        <Content><h1>he </h1>content</Content>
+     </Layout>
+   </Layout>
   );
 }
 
